1e61bd725b09dc3aa4837786f9a4e4b05516f808
jellybeanci <gokselkucuksahin@gmail.com>
2021-08-02 16:29:18 +0300

    Basic Setup p5js and Vue.js

---
+++ docs/sketch.js
@@ -0,0 +1,14 @@
+const width = 600;
+const height = 600;
+
+function setup() {
+  const canvas = createCanvas(width, height);
+  canvas.parent("p5canvas");
+  background(64, 128, 128);
+  colorCounter = 0;
+  colorMode(HSB, 1);
+  strokeWeight(5);
+}
+
+function draw() {
+}
