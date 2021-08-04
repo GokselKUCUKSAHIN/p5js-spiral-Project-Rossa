8147ae68c1aab376a046b98bd3c9561c7e7da6bb
jellybeanci <gokselkucuksahin@gmail.com>
2021-08-04 12:26:23 +0300

    Radius set as parametrik.

--- docs/script/sketch.js
+++ docs/script/sketch.js
@@ -16,19 +16,19 @@ function draw() {
   translate(width * 0.5, height * 0.5);
   ellipse(0, 0, 10, 10);
   background(51);
-  drawSpiral(vueInstance.fraction, vueInstance.numberOfPoints, vueInstance.dotsize);
+  drawSpiral(vueInstance.fraction, vueInstance.numberOfPoints, vueInstance.dotsize, R);
 }
 
 function drawBall(x, y, size) {
   ellipse(x, y, size);
 }
 
-function drawSpiral(fraction, numberOfPoints, dotsize) {
+function drawSpiral(fraction, numberOfPoints, dotsize, radius) {
   for (let i = 0; i < numberOfPoints; i++) {
     const dist = i / (numberOfPoints - 1);
     const angle = TWO_PI * fraction * i;
-    const x = dist * cos(angle) * R;
-    const y = dist * sin(angle) * R;
+    const x = dist * cos(angle) * radius;
+    const y = dist * sin(angle) * radius;
     drawBall(x, y, dotsize);
   }
 }
